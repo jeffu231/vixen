fea8a0b4e94411d2a420ec2719a8d6497b61287f
jon@chuchla.com <jon@chuchla.com>
2019-01-01 13:29:42 -0600

    VIX-2705 Initial Commit.  Added Data/Value/BitmapValue.cs

---
+++ Vixen.System/Data/Value/BitmapValue.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace Vixen.Data.Value
+{
+    public struct BitmapValue : IIntentDataType
+	{
+		public BitmapValue(Bitmap image)
+		{
+			Image = image;
+		}
+
+		/// <summary>
+		/// A System.Drawing.Bitmap object
+		/// </summary>
+		public Bitmap Image;
+	}
+}
